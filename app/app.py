4d6c0942be58f39e90f557a334bc0ede701e187e
mac <mac@l3shans-MacBook-Pro.local>
2026-02-05 12:43:22 +0300

    fixed app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins + Argo CD in gitops namespace 🚀"
+    return "Hello from Jenkins + Argo CD in gitops namespace "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
